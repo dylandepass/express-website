e155056c4f67e37472898800c79722e33cd96102
Dylan Depass <dylandepass@gmail.com>
2022-09-25 23:59:38 -0400

    feat: add chromatic + font size change

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 export const parameters = {
   host: 'https://main--helix-playground--dylandepass.hlx.page',
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -6,5 +8,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
   },
 }
